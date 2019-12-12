ce3babf8d1f84c83bad9069287e378afd758e6a7
Benito Galvez <mystwalker131@gmail.com>
2019-12-12 04:37:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,8 @@ end
  
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
 introduction_with_language("Dan, Ember.js")
 
 introduction_with_language("Dan,Ember.js")
